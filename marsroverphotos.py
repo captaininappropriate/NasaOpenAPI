a80867c1ea4f1d389680cef795b8b0c225cc432e
captaininappropriate <gregnimmo@protonmail.com>
2025-09-22 16:39:56 +1000

    setup dynamic list updating for cameras based on rover

--- marsroverphotos.py
+++ marsroverphotos.py
@@ -1,6 +1,7 @@
 from PyQt6.QtWidgets import (
     QDialog, 
     QHBoxLayout,
+    QVBoxLayout,
     QCalendarWidget,
     QListWidget,
     QLabel,
@@ -25,12 +26,28 @@ class MarsRoverPhotosDialog(QDialog):
         
         # add items to the lists
         # rover list
-        rovers = ['Curiosity','Opportunity', 'Spirit']
-        for rover in rovers:
-            self.rover_list.addItem(rover)
+        self.rover_list.addItems(['Curiosity','Opportunity', 'Spirit'])
+        self.rover_list.currentItemChanged.connect(self.update_camera_list)
 
-        # camera list
-        cameras = {
+        layout.addWidget(self.calendar)
+        layout.addWidget(self.rover_list)
+        layout.addWidget(self.camera_selection_list)
+        self.setLayout(layout)
+        
+        # initial list of items
+        self.update_camera_list()
+
+    def update_camera_list(self):
+        self.camera_selection_list.clear()
+
+        # get the current rover
+        current = self.rover_list.currentItem()
+
+        if not current:
+            return
+
+        # curiosity camera list
+        curiosity_cameras = {
             "FHAZ": "Front Hazard Avoidance Camera",
             "RHAZ": "Rear Hazard Avoidance Camera",
             "MAST": "Mast Camera",
@@ -38,17 +55,25 @@ class MarsRoverPhotosDialog(QDialog):
             "MAHLI": "Mars Hand Lens Imager",
             "MARDI": "Mars Descent Imager",
             "NAVCAM": "Navigation Camera",
+        }
+
+        # opportunity & spirit camera list
+        opportunity_spirit_cameras = {
+            "FHAZ": "Front Hazard Avoidance Camera",
+            "RHAZ": "Rear Hazard Avoidance Camera",
+            "NAVCAM": "Navigation Camera",
             "PANCAM": "Panoramic Camera",
             "MINITES": "Miniature Thermal Emission Spectrometer (Mini-TES)",
         }
-        for value in cameras.values():
-            self.camera_selection_list.addItem(value)
-
-        layout.addWidget(self.calendar)
-        layout.addWidget(self.rover_list)
-        layout.addWidget(self.camera_selection_list)
-        self.setLayout(layout)
         
+        # select appropriate camera list
+        # curiosity
+        if current.text() == "Curiosity":
+            for value in curiosity_cameras.values():
+                self.camera_selection_list.addItem(value)
+        else:  # "opportunity & spirit
+            for value in opportunity_spirit_cameras.values():
+                self.camera_selection_list.addItem(value)
 
 
 
